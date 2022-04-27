05c5330018c6b4b72969abfc9292e84a6d56c1a7
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-27 16:51:58 -0500

    Update test_home.py

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -41,7 +41,7 @@ def test_estimate_route(app, client):
     THEN check that the response is valid
     """
     with app.test_client() as test_client:
-    estimate = {"radius":"180", "height":"360"}
+        estimate = {"radius":"180", "height":"360"}
     res = test_client.post('/estimate', data=estimate)
     assert res.status_code == 200
     assert b"The estimate for painting is $143100" in res.data
