0762ee0d2a8195179a7ef5cd343986c9850c396e
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-27 16:59:48 -0500

    update

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -34,17 +34,17 @@ def test_estimate_route(app, client):
         assert res.status_code == 200
         assert b"Estimator" in res.data
 
-#def test_estimate_route(app, client):
-  #  """
-  #  GIVEN a Flask application configured for testing
-  #  WHEN the '/estimate' route is requested (POST)
-  #  THEN check that the response is valid
-  #  """
-  #  with app.test_client() as test_client:
-  #  estimate = {"radius":"180", "height":"360"}
-  #  res = test_client.post('/estimate', data=estimate)
-  #  assert res.status_code == 200
-  #  assert b"The estimate for painting is $143100" in res.data
+def test_estimate_route(app, client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/estimate' route is requested (POST)
+    THEN check that the response is valid
+    """
+    with app.test_client() as test_client:
+    estimate = {"radius":"180", "height":"360"}
+    res = test_client.post('/estimate', data=estimate)
+    assert res.status_code == 200
+    #assert b"The estimate for painting is $143100" in res.data
 
 
   
